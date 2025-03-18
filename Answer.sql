c2b62f124b248c191872fd0a268d87ed7f88181c
akaJoe <akaj35179@gmail.com>
2025-03-18 02:05:40 +0100

    project added

---
+++ Answer.sql
@@ -0,0 +1,23 @@
+Create database SalesDB (
+    SalesID int,
+    SalesDate date,
+    SalesAmount float,
+    SalesTax float,
+    SalesTotal float,
+    SalesComments varchar(255),
+);
+
+Create tabele SalesDB.Sales (
+    SalesID int,
+    SalesDate date,
+    SalesAmount float,
+    SalesTax float,
+    SalesTotal float,
+    SalesComments varchar(255)
+);
+
+
+Drop database demo (
+
+);
+
